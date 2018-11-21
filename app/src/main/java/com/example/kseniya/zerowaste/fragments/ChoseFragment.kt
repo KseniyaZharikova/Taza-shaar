4f3b95e52e9b4b742f873fc0109e97ff6a74f043
KseniyaZharikova <kseniya.zharikova.viktorovna@gmail.com>
2018-11-21 21:46:48 +0600

    add gestureListener

--- app/src/main/java/com/example/kseniya/zerowaste/fragments/ChoseFragment.kt
+++ app/src/main/java/com/example/kseniya/zerowaste/fragments/ChoseFragment.kt
@@ -1,21 +1,52 @@
 package com.example.kseniya.zerowaste.fragments
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.example.kseniya.zerowaste.R
+import com.example.kseniya.zerowaste.utils.GestureListener
 
 
-class ChoseFragment : BaseFragment() {
-
+class ChoseFragment : BaseFragment(), GestureListener.Callback  {
+    private var gestureListener: GestureListener? = null
     override fun getViewLayout(): Int {
         return R.layout.fragment_chose
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun clearProgressAnimations(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun animateCurrentStatus() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun collapseView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun expandView() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun changeAlpha(alpha: Float) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getY(): Float {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setY(y: Float) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResume() {
+        super.onResume()
+
     }
 
 }
