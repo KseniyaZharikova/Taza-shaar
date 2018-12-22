3c048650c1264bd7c1760d0253382acd490bdcaa
KseniyaZharikova <kseniya.zharikova.viktorovna@gmail.com>
2018-12-22 21:17:40 +0600

    fixed design

--- app/src/main/java/com/example/kseniya/zerowaste/utils/Constants.java
+++ app/src/main/java/com/example/kseniya/zerowaste/utils/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
     public static final int LOCATION_REQUEST_CODE = 100;
 
     //BISHKEK_LOCATION
-    public static final double LAT = 42.845440;
-    public static final double LNG = 74.608897;
+    public static final double LAT = 42.8748635;
+    public static final double LNG = 74.6048324;
 
     //OFFLINE_AREA
     public static final double LAT_SW = 42.79755;
